8230f87c7e6c43802e928bc1aa2eab7a6db74fee
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2024-09-19 18:05:54 +0100

    works but not enough

--- test_locust/my_user.py
+++ test_locust/my_user.py
@@ -4,16 +4,20 @@ from locust.stats import stats_printer
 from locust import HttpUser, TaskSet, task
 import gevent
 
+
 class UserBehavior(TaskSet):
     @task
     def my_task(self):
         self.client.get("/")
 
+
 class MyUser(HttpUser):
+    host = "http://localhost:8000"
     tasks = [UserBehavior]
 
     def wait_time(self):
         return 1
+
 
 def main():
     setup_logging("INFO", None)
@@ -29,11 +33,12 @@ def main():
     env.runner.start(1, spawn_rate=1)  # Start with 1 user and 1 spawn rate
 
     # Run for a certain duration (e.g., 10 seconds)
-    gevent.spawn_later(10, lambda: env.runner.quit())
+    gevent.spawn_later(1000, lambda: env.runner.quit())
     env.runner.greenlet.join()
 
     # Stop Locust environment
     print("Test finished!")
+
 
 if __name__ == "__main__":
     main()
